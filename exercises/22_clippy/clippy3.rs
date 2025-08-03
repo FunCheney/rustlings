e8af86b829b3cc829b25e009b6063acc750873c0
Fchen <fc_2012@163.com>
2025-08-03 11:15:54 +0800

    fix: clippy3

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -2,10 +2,10 @@
 // TODO: Fix all the Clippy lints.
 
 #[rustfmt::skip]
-#[allow(unused_variables, unused_assignments)]
+#[allow(unused_variables)]
 fn main() {
     let my_option: Option<&str> = None;
-    // Correctly handle Some case
+    // Safely handle Option using pattern matching
     if let Some(val) = my_option {
         println!("{}", val);
     }
@@ -23,7 +23,7 @@ fn main() {
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Properly swap values
-    std::mem::swap(&mut value_a, &mut value_b);
+    // Properly swap values using tuple assignment
+    (value_a, value_b) = (value_b, value_a);
     println!("value a: {value_a}; value b: {value_b}");
 }
